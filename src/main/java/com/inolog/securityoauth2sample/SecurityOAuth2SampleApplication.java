9ab19b8f60a630a4a600e3829a9684a40cf6fd45
Ino <sylee74133@gmail.com>
2024-08-08 01:42:45 +0900

    first commit

---
+++ src/main/java/com/inolog/securityoauth2sample/SecurityOAuth2SampleApplication.java
@@ -0,0 +1,13 @@
+package com.inolog.securityoauth2sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOAuth2SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOAuth2SampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inolog/securityoauth2sample/SecurityOAuth2SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inolog.securityoauth2sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOAuth2SampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
