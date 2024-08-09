228710dd71f9dd56b865f1c4a8439294c15d9b6c
Ino <sylee74133@gmail.com>
2024-08-09 18:05:54 +0900

    update README.md

--- src/main/java/com/securityoauth2sample/SecurityOAuth2SampleApplication.java
+++ src/main/java/com/securityoauth2sample/SecurityOAuth2SampleApplication.java
@@ -1,4 +1,4 @@
-package com.inolog.securityoauth2sample;
+package com.securityoauth2sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
